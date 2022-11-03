015b341dde112519860ca389070517a012d929bb
Adi Campan <adicampan1974@gmail.com>
2022-11-03 19:21:46 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -4,35 +4,41 @@ import Card from './Components/Card';
 import { v4 as uuidv4 } from 'uuid';
 import { useState, useEffect } from 'react';
 import { cats } from './ListOfCats';
-import reverseCard from './Components/Assets/icon.jpg';
-import catEars from './Components/Assets/cat-ears-2.png';
-// const backCover = cats.map((cat) => {
-  //   (cat.image = reverseCard)
-  //   return cat
-  // });
 
 function App() {
-  
-  useEffect(() => {
-    setListOfCats(cats.sort(() => Math.random() - 0.5));
-  }, []);
+  const randomList= () => cats.sort(() => Math.random() - 0.5)
+  const [listOfCats, setListOfCats] = useState(randomList);
+  const [hideCats, setHideCats] = useState(false);
 
-  const [listOfCats, setListOfCats] = useState([])
+  useEffect(() => {
+    setListOfCats(randomList)
+    const hide = () => {
+      setHideCats(true)
+    };
+    setTimeout(hide, 2000);
+  }, [listOfCats]);
 
   const startNewGame = () => {
-    const newRandomList = cats.sort(() => Math.random() - 0.5)
-    setListOfCats(newRandomList);
+    setListOfCats(randomList)
+    const hide = () => {
+      setHideCats( ()=> true)
+    };
+    setTimeout(hide, 2000);
   };
 
- console.log(listOfCats)
- 
   return (
     <div className='app'>
       <h1 className='title'>FIND THE PAIRS</h1>
       <button onClick={() => startNewGame()}>Start New Game</button>
       <div className="game">
         {listOfCats?.map((cat) =>
-          <Card key={cat.id} image={cat.image} name={cat.name} />
+          <Card
+            listOfCats={listOfCats}
+            key={cat.id}
+            id={cat.id}
+            image={cat.image}
+            hideCats={hideCats}
+          />
         )}
       </div>
     </div>
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Card.css';
 
-const Card = ({ image, name }) => {
+const Card = ({ image, id,  hideCats, listOfCats }) => {
+  const [view, setView] = useState(false);
+  const [firstImage, setFirstImage] = useState();
+  const [secondImage, setSecondImage] = useState();
+
+  const showImage = (id) => {
+    const firstCat = listOfCats.filter((cat) => cat.id === id)
+    const secondCat = listOfCats.filter((cat) => cat.id === id)
+     setView(true)
+     setFirstImage(firstCat[0].image)
+     setSecondImage(secondCat[0].image)
+     console.log((firstCat[0].image),(secondCat[0].image));
+  };
+  
+  
 
   return (
-    <div className='card'>
-      <img className='image' src={image}/>
+    <div onClick={() => showImage(id)} className={view? 'hide-card': 'card'}>
+      {/* {view && <img src={cat}/>} */}
+      <img className={hideCats && !view ? 'image-hide' : 'image'} src={image} />
     </div>
   )
-}
+};
 
 export default Card;
